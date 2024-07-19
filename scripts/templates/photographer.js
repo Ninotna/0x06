1e2562af07c44b73d921323b94c08aeefd954d8e
Antonin @dev87 <antonin@dev87.fr>
2024-07-19 18:38:09 +0200

    import des data provenant du JSON. Préparation du code html de réception. Code JS en conséquence pour la homepage. Prochaine étape: revoir le css de la de la homepage et l'accessibilité du site

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,5 +1,5 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait } = data;
 
     const picture = `assets/photographers/${portrait}`;
 
@@ -9,8 +9,24 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const pLocation = document.createElement( 'p' );
+        pLocation.setAttribute("class", "ph_location");
+        pLocation.textContent = `${city}, ${country}`;
+
+        const pTagline = document.createElement( 'p' );
+        pTagline.setAttribute("class", "ph_tagline");
+        pTagline.textContent = tagline;
+
+        const pPrice = document.createElement( 'p' );
+        pPrice.setAttribute("class", "ph_price");
+        pPrice.textContent = `${price}€/jour`;
+
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(pLocation);
+        article.appendChild(pTagline);
+        article.appendChild(pPrice);
+
         return (article);
     }
     return { name, picture, getUserCardDOM }
