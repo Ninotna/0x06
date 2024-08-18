5e6a81c4f8544f467b2fc8f34fb5acd245f83b24
ninotna <antonin@dev87.fr>
2024-08-18 20:19:29 +0200

    test debugging erreur 404 github

--- js/components/PhotographerUiManager.js
+++ js/components/PhotographerUiManager.js
@@ -1,7 +1,7 @@
 import PhotographerProfileTemplate from "../templates/photographerProfile.js";
 import { PhotographerFactory } from "../factories/media.js";
-import { PhotographerData } from "../utils/PhotographerData.js"
 import { PhotographerEventHandler } from '../utils/PhotographerEventHandler.js';
+import { PhotographerData } from "../utils/PhotographerData.js";
 
 
 export class PhotographerUIManager {
